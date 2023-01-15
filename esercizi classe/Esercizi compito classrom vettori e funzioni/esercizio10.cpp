c56fefa2c1fd34066854ca6893b1f8f0f4badf44
Ignazio <ignazioandsperandeo@gmail.com>
2023-01-16 00:44:26 +0100

    .

--- esercizi classe/Esercizi compito classrom vettori e funzioni/esercizio10.cpp
+++ esercizi classe/Esercizi compito classrom vettori e funzioni/esercizio10.cpp
@@ -1,3 +1,54 @@
-//
-// Created by Ignazio on 15/01/2023.
-//
+#include <iostream>
+using namespace std;
+void sorting(int[], int);
+void sorting(int[][100], int, int);
+int main(){
+    int r;
+    int c;
+    cout << "Inserisci le righe: ";
+    cin >> r;
+    cout << "Inserisci le colonne: ";
+    cin >> c;
+    int matrix[r][c];
+    sorting(matrix, r, c);
+    for (int i = 0;i < r;i++){
+        for (int j = 0;j < c;j++){
+            cout << "Inserisci il valore alla riga " << i << " e alla colonna " << j << " : ";
+            cin >> matrix[i][j];
+            cout << endl;
+        }
+    }
+    for (int i = 0;i < r;i++){
+        sorting(matrix[i], c);
+    }
+    for (int i = 0;i < r;i++){
+        for (int j = 0;j < c;j++){
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+void sorting(int vet[], int len){
+    for (int i = 0;i < len;i++){
+        for (int j = i + 1;j < len;j++){
+            if (vet[i] > vet[j]){
+                int temp = vet[i];
+                vet[i] = vet[j];
+                vet[j] = temp;
+            }
+        }
+    }
+}
+void sorting(int mat[][100], int r, int c){
+    for (int i = 0;i < c;i++){ // ci vuole un ulteriore ciclo perchè ci serve tenere fermo il valore di ogni cella per k volte in modo da confrontare tutte le possibili coppie delle celle.
+        for(int j = 0;j < r;j++){
+            for (int k = j + 1;k < r;k++) {
+                if (mat[j][i] > mat[k][i]) {
+                    int temp = mat[j][i];
+                    mat[j][i] = mat[k][i];
+                    mat[k][i] = temp;
+                }
+            }
+        }
+    }
+}
