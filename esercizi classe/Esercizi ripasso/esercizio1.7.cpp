72cd80d43990c2e963b1170c7a0949e398dd3342
Ignazio <ignazioandsperandeo@gmail.com>
2023-01-18 22:20:38 +0100

    .

---
+++ esercizi classe/Esercizi ripasso/esercizio1.7.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int sum(int, int);
+int main(){
+    int n;
+    int j = 2;
+    cout << "Inserisci la lunghezza della successione di Fibonacci: ";
+    cin >> n;
+    int succ[n];
+    succ[0] = 0;
+    succ[1] = 1;
+    for (int i = 0;i < n;i++){
+        succ[j] = sum(succ[j-1], succ[j-2]);
+        j ++;
+    }
+    for (int i = 0;i < n;i++) {
+        cout << succ[i] << " ";
+    }
+}
+int sum(int a , int b){
+    return a + b;
+}
