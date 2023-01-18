063a2ac35551643c821144178832e1d7cc48e18f
Ignazio <ignazioandsperandeo@gmail.com>
2023-01-18 19:41:52 +0100

    .

---
+++ esercizi classe/Esercizi ripasso/esercizio1.6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void control(int*, int*, int*);
+int main(){
+    int im;
+    int im1;
+    int im2;
+    cout << "Inserisci i 3 importi: ";
+    cin >> im >> im1 >> im2;
+    control(&im, &im1, &im2);
+    cout << "Conti: " << im << " " << im1 << " " << im2 << endl;
+    return 0;
+}
+void control(int *a, int *b, int *c){
+    if (*b < 0){
+        *a = *a + *b;  // metto + perchè se b è negativo e mettessi - succederebbe questo: *a = *a - (-*b) quindi si sommeranno
+        *b = 0;
+    }
+    if (*c < 0){
+        *a = *a + *c;
+        *c = 0;
+    }
+}
