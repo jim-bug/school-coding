1e9c6e7671e9943c368b5574a2bbeabbde93ebe2
jim-bug <ignazioandsperandeo@gmail.com>
2023-04-28 09:03:34 +0200

    .

--- Cpp/esercizi classe/file exercise/esercizio8.8(file)/esercizio8.8(file).cpp
+++ Cpp/esercizi classe/file exercise/esercizio8.8(file)/esercizio8.8(file).cpp
@@ -1,39 +1,47 @@
 #include <iostream>
 #include <fstream>
+#include <ctime>
+#include <cstdlib>
 #define L 10
 #define L1 3
+int controls(int[]);
 using namespace std;
 int main(){
     // pag A248 n 27:
-
-    ifstream file_in("navi.txt");
+    srand(time(0));
+    ofstream file_ou("navi.txt");
     ifstream file_hit("colpi.txt");
-    ifstream file_hit_1("colpi.txt");
     int mat[L][L];
-    int supp;
-    int len;
     int r;
     int c;
     int colpi = 0;
     int navi_aff = 0;
     int vet[L1];
+    int control[L1];
+    int value;
+    int end = 3;
+    int start = 0;
+    int supp;
     for (int i = 0;i < L1;i++){
         vet[i] = 0;
     }
     for (int i = 0;i < L;i++){
         for (int j = 0;j < L;j++){
-            file_in >> supp;
-            mat[i][j] = supp;
+            value = (rand()%(end+1-start)+start);
+            control[value-1] ++;
+            supp = controls(control);
+            if (supp != 0){
+                while (value == supp){
+                    value = (rand()%(end+1-start)+start);
+                }
+            }
+            mat[i][j] = value;
+            file_ou << mat[i][j] << " ";
         }
+        file_ou << endl;
     }
-    while (not file_hit.eof()){
-        file_hit >> supp;
-        len ++;
-    }
-    len /= 2;   // ricavo il numero di colpi
-    for (int i = 0;i < len;i++){
-        file_hit_1 >> r;
-        file_hit_1 >> c;
+    while(file_hit >> r){
+        file_hit >> c;
         if (mat[r][c] >= 1 and mat[r][c] <= 3){
             colpi ++;
             vet[mat[r][c]-1] ++;
@@ -45,8 +53,15 @@ int main(){
         }
     }
     cout << "Colpi: " << colpi << endl << "Navi affondate: " << navi_aff << endl;
-    file_in.close();
+    file_ou.close();
     file_hit.close();
-    file_hit_1.close();
+    return 0;
+}
+int controls(int vet[]){
+    for (int i = 0;i < L1;i++){
+        if (vet[i] == i + 1){
+            return vet[i];
+        }
+    }
     return 0;
 }
