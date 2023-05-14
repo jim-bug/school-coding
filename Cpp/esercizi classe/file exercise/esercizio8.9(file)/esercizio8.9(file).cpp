5656b24601f32fcee1dd17fed9297df6ee95ceb6
jim-bug <ignazioandsperandeo@gmail.com>
2023-05-14 11:26:12 +0200

    .

--- Cpp/esercizi classe/file exercise/esercizio8.9(file)/esercizio8.9(file).cpp
+++ Cpp/esercizi classe/file exercise/esercizio8.9(file)/esercizio8.9(file).cpp
@@ -12,24 +12,29 @@ int main(){
     ifstream file("risultati.csv");
     string temp;
     char support;
-    int index = 0;
+    int index = -1;
     int count_div = -1;
     string data;
+    string chars;
     int n_people = 0;
-    while (file >> support){
+    while (file.eof()){
+        file >> support;
         if (int(support) >= 65 && int(support) <= 90){
             n_people ++;
         }
         data.push_back(support);
     }
+    // getline(file, chars);
+    cout << 5;
+    /*
     Alunno alunni[n_people];
     for (int i = 0;i < data.size();i++){
-        if (i > 0 && int(support) >= 65 && int(support) <= 90){
+        if (int(support) >= 65 && int(support) <= 90){
             index ++;
             count_div = -1;
         }
         else{
-            while (data[i] != ','){
+            while (data[i] != ',' || (i > 0 &&  not int(data[i]) >= 65 && int(data[i]) <= 90)){
                 temp.push_back(data[i]);
                 i ++;
             }
@@ -52,14 +57,14 @@ int main(){
             temp.clear();
         }
     }
-
-    for (int i = 0;i < n_people;i++){
+*/
+  /*  for (int i = 0;i < n_people;i++){
         cout << alunni[i].nome << endl;
         cout << alunni[i].matricola << endl;
         cout << alunni[i].eta << endl;
         cout << alunni[i].voto << endl;
         cout << endl;
-    }
+    }*/
     file.close();
     return 0;
 }
