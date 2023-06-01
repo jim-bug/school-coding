12b5a5b308965f327c0c1ee851cf5e3e0da81f7c
Ignazio <ignazioandsperandeo@gmail.com>
2023-06-01 06:49:08 +0200

    .

--- cmd/sesto_script.cmd
+++ cmd/sesto_script.cmd
@@ -1,8 +1,23 @@
-rem echo Calendario aprile maggio giugno 2023 > "Calendario.txt"
+@echo off
+setlocal enabledelayedexpansion
 
-for %%i in (apr, magg, giugn) do (
-    rem echo lun mar mer gio ven sab dom >> "Calendario.txt"
-    for /L %%j in (1-1-30) do (
-        echo %%j >> Calendario.txt
-    ) 
+rem set days="Lunedì Martedì Mercoledì Giovedì Venerdì Sabato Domenica"
+set numbers[0]=31
+set numbers[1]=30
+set numbers[2]=31
+set numbers[3]=31
+set numbers[4]=30
+set counter=0
+set index=0
+for %%m in (maggio giugno luglio agosto settembre) do (
+    echo %%m
+    for /L %%j in (1, 1, !numbers[%index%]!) do (
+        for %%i in (Lunedi Martedi Mercoledi Giovedi Venerdi Sabato Domenica) do (
+            if !counter! LSS 31 (
+                echo %%i !counter!
+            )
+            set /a counter+=1
+        ) 
+    )
+    set /a index+=1 
 )
