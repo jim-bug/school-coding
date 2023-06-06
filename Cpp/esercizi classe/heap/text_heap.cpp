c5ad5722ce2a5b78fe8bcdb914fffe10e8654d32
jim-bug <ignazioandsperandeo@gmail.com>
2023-06-06 15:00:48 +0200

    .

---
+++ Cpp/esercizi classe/heap/text_heap.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    // pila:la pila è una lista di lunghezza variabile, LIFO, ultimo a entare primo a uscire
+    // coda: la coda è una lista di lunghezza variabile. FIFO, primo a entrare primo a uscire
+    // con pila e coda non si parla più di 
+    return 0;
+}
