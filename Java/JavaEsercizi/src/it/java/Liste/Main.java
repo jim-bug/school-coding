34dc174e3ff9cc76e88e303cb944c1993efd24b8
jim-bug <ignazioandsperandeo.com>
2023-11-08 17:11:23 +0100

    .

---
+++ Java/JavaEsercizi/src/it/java/Liste/Main.java
@@ -0,0 +1,12 @@
+package it.java.Liste;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Lista lista = new Lista();
+		lista.inserisci("ciao");
+		lista.inserisci("come");
+
+	}
+
+}
