bdc4bf350a4abe105958dbcd43dae6838ae627ba
jim-bug <ignazioandsperandeo.com>
2023-12-02 15:43:30 +0100

    .

--- Bash/SonoroScript1/1.sh
+++
@@ -1,7 +0,0 @@
-ssh alunni@l415l00 "bash -s <<paperino
-hostname
-echo ciao
-whoami
-paperino
-"
-
--- Bash/SonoroScript1/scriptSonoro.sh
+++ Bash/SonoroScript1/scriptSonoro.sh
@@ -6,19 +6,8 @@
 # Alex Chirvasa
 # Alessandro Cangiamila
 
-localUser="alunni"
-remoteHost="l415l00"
+remoteUser="alunni"
 remotePath="/var/www/html/pub/catalogo_sonoro/"
-localUser1="4C"
-remoteHost1="l415l01"
-destinationPath="/home/4C/Scaricati/SonoroScript/CatalogoSonoro"
-export localPath=\"/var/www/html/pub/catalogo_sonoro\"
-remotePass="Albicocca"
-mkdir -p CatalogoSonoro 
-echo "Autenticazione per l'esecuzione: "
-sshpass -p "alunni" ssh $localUser@$remoteHost " bash -x -s << prova 
-cd $localPath
-file * \| grep -E '44100 Hz|44.1 kHz' \| cut -d: -f\1 \| xargs \-I \{} sshpass -p $remotePass scp {} $localUser@$remoteHost:$destinationPath
-prova
-"
-ls CatalogoSonoro
+remotePass="alunni"
+remoteHost="l415l00"
+sshpass -p $remotePass ssh $remoteUser@$remoteHost "cd $remotePath ; file * | grep -E '44100 Hz|44.1 kHz' | cut -d: -f1"
