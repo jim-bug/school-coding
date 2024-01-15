82982856b996bd722fcf1bc23348acb316fbfac9
Ignazio <ignazioandsperandeo@gmail.com>
2024-01-15 23:14:43 +0100

    .

--- JavaTeP/test/it/java/release1/RettangoloTest.java
+++ JavaTeP/test/it/java/release1/RettangoloTest.java
@@ -9,10 +9,19 @@ public class RettangoloTest {
 	@Test
 	public void testGetArea() {
 		String larghezza = "-10";
-		String lunghezza = "-1";
+		String lunghezza = "1";
 		double area = Double.parseDouble(larghezza)*Double.parseDouble(lunghezza);
 		Rettangolo stanza = new Rettangolo(lunghezza, larghezza);
 		assertEquals("Input non valido", area, stanza.getArea(), 0.001);
+	}
+	
+	@Test
+	public void testGetPerimetro() {
+		String larghezza = "10";
+		String lunghezza = "4";
+		double perimetro = (Double.parseDouble(larghezza)+Double.parseDouble(lunghezza))*2;
+		Rettangolo stanza = new Rettangolo(lunghezza, larghezza);
+		assertEquals("Input non valido", perimetro, stanza.getPerimetro(), 0.001);
 	}
 
 
