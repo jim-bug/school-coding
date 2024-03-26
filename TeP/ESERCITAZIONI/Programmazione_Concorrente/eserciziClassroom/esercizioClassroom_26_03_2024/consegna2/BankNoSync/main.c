99ae1e8eb64bd4d1a641e293dee7d4358144cb0c
jim_bug <ignazioandsperandeo@gmail.com>
2024-03-27 00:52:04 +0100

    Aggiunta esercizi TeP di classroom del 26 marzo 2024.

---
+++ TeP/ESERCITAZIONI/Programmazione_Concorrente/eserciziClassroom/esercizioClassroom_26_03_2024/consegna2/BankNoSync/main.c
@@ -0,0 +1,59 @@
+/* Pthread example - bank.c               */
+/* Author: Marco Frison for Mr. Webmaster */
+/* Released under GPL v3                  */
+/* 20151108 AT modified for edu           */
+/* Programmazione concorrente in POSIX C  */
+/* http://www.mrwebmaster.it/c/programmazione-concorrente-posix-c_7688.html */
+/* esempio senza sincronizzazione */
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include "worker.h"
+
+int main(int argc, char *argv[])
+{
+
+  int money = 0;
+  int n_threads, i;
+  pthread_t *threads;
+  thread_args *args;
+
+  // check parameters
+  if (argc != 2)
+  {
+    printf("Error - Three parameters required\n");
+    printf("[USAGE]: bank <N threads>\n");
+    exit(1);
+  }
+  // get threads number
+  n_threads = atoi(argv[1]);
+  if (n_threads < 1)
+  {
+    printf("Threads parameter must be greater than one!\n");
+    exit(-1);
+  }
+
+  // allocate threads and their args
+  threads = (pthread_t*) malloc(sizeof(pthread_t) * n_threads);
+  args = (thread_args*) malloc(sizeof(thread_args) * n_threads);
+
+  // prepare and starts threads
+  for (i = 0; i < n_threads; i++)
+  {
+    (args + i)->index = i;
+    (args + i)->money = &money;
+    pthread_create((threads + i), NULL, worker, (void *)(args + i));
+  }
+
+  // wait threads die
+  for (i = 0; i < n_threads; i++)
+  {
+    pthread_join(*(threads + i), NULL);
+  }
+
+  // print result
+  printf("Account state: %d\n", money);
+  exit(0);
+
+}
---
+++ TeP/ESERCITAZIONI/Programmazione_Concorrente/eserciziClassroom/esercizioClassroom_26_03_2024/consegna2/BankNoSync_Correzione/worker.c
@@ -0,0 +1,52 @@
+/* Pthread example - worker.c             */
+/* Author: Marco Frison for Mr. Webmaster */
+/* Released under GPL v3                  */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include "worker.h"
+
+#define BSIZE 10
+#define bzero(b,len) (memset((b), '\0', (len)), (void) 0)
+/*
+void* bzero(char *buf,int bs)
+{
+	memset(buf,0x0,bs);
+}
+*/
+/* Worker thread */
+void worker(void* args)
+{
+
+  char target[BSIZE + 1];
+  char buffer[BSIZE + 1];
+  int index;
+  FILE* file;
+  int* money;
+    
+  // get thread args
+  index = ((thread_args*) args)->index;
+  money = ((thread_args*) args)->money;
+  
+  bzero(buffer, BSIZE);
+  snprintf(target, BSIZE, "%s%d", "cash", index);
+  
+  file = fopen(target, "r");
+  if (file == NULL)
+  {
+    printf("Error - Bad cash file %s!\n", target);
+    exit(-2);
+  }
+  
+  // read all the rows of the file
+  while (fgets(buffer, BSIZE, file) != NULL)
+  {
+    // update money value
+    *money = *money + atoi(buffer);
+    bzero(buffer, BSIZE);
+  }
+
+  pthread_exit(NULL);
+}
