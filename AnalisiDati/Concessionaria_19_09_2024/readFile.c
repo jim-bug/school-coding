7db9ffc5caeb19778825b257cd58e8ebe84071d9
jim_bug <ignazioandsperandeo@gmail.com>
2024-09-29 23:05:57 +0200

    .

--- AnalisiDati/Concessionaria_19_09_2024/readFile.c
+++ AnalisiDati/Concessionaria_19_09_2024/readFile.c
@@ -6,10 +6,8 @@
 void printRecord(char record, char field, char line[]);
 
 int main() {
-    // Apri il file per la lettura
     FILE* file = fopen("./Record_Cliente.txt", "r");
 
-    // Verifica se il file è stato aperto correttamente
     if (file == NULL) {
         perror("Errore nell'apertura del file");
         return EXIT_FAILURE;
@@ -19,7 +17,6 @@ int main() {
     char record = '\n'; // Separatore di record (nuova riga)
     char field = '=';   // Separatore di campo (uguale)
 
-    // Leggi il file riga per riga
     while (fgets(line, sizeof(line), file)) {
         printRecord(record, field, line);
     }
@@ -34,15 +31,11 @@ void printRecord(char record, char field, char line[]) {
     int len = strlen(line);
     char fie[LENGTH];  // Buffer per memorizzare il campo
 
-    // Itera attraverso ogni carattere della linea
     for (int i = 0; i <= len; i++) {
-        // Se trovi il separatore di campo o la fine della linea
         if (line[i] == field || line[i] == record || line[i] == '\0') {
-            // Copia il campo estratto nella stringa 'fie'
             strncpy(fie, line + pointer, i);
-            fie[i - pointer] = '\0';  // Termina la stringa con \0
-            printf("%s\n", fie);      // Stampa il campo estratto
-            pointer = i + 1;          // Aggiorna il puntatore per il prossimo campo
+	    fie[i-pointer] = '\0';
+            pointer = i + 1;
         }
     }
 }
