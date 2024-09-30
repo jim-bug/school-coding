5391c3cf934b98309e981475387bae37814e87a0
jim_bug <ignazioandsperandeo@gmail.com>
2024-09-30 06:53:47 +0200

    .

--- AnalisiDati/Concessionaria_19_09_2024/Codici di lettura record/readFile.py
+++ AnalisiDati/Concessionaria_19_09_2024/Codici di lettura record/readFile.py
@@ -1,3 +1,7 @@
+# Author: Ignazio Leonardo Calogero Sperandeo
+# Date: 30/09/2024
+# Project: Sperimentazione con file organizzati sequenzialmente in python
+# by jim_bug :)
 
 date = ["Nome: ", "Cognome: ", "Data: ", "Sesso: ", "Codice Fiscale: ", "Stato: ", "Regione: ", "Provincia: ", "Citta: ", "Residenza: ", "Civico: ", "CAP: ", "TEL: "]
 with open("./Record_Cliente.txt", 'r') as file:
