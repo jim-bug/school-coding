89ce5f5cfa6d7ab94efecd18c75516a256c56d88
jim_bug <ignazioandsperandeo@gmail.com>
2024-10-11 06:52:06 +0200

    Ho effettuato migliorie nella JimShell, ho implementato l'uso del & process. Ho risolto i problemi legato al cambio di directory.

--- TeP/ESERCITAZIONI/Programmazione_Concorrente/jimShell/jsh.c
+++ TeP/ESERCITAZIONI/Programmazione_Concorrente/jimShell/jsh.c
@@ -1,3 +1,12 @@
+/*
+ * Consegna: Realizzare in C una shell.
+ * Data: 29/02/2024
+ * Revisione: 11/10/2024
+ * Autore: Ignazio Leonardo Calogero Sperandeo
+ * 
+ * by jim_bug:)
+ * */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -7,16 +16,11 @@
 #define MAX_LENGTH_COMMAND 50
 #define MAX_LENGTH_PATH 1024
 #define MAX_LENGTH_USER_PATH 50
-/*
- * Consegna: Realizzare in C una shell.
- * Data: 29/02/2024
- * Autore: Ignazio Leonardo Calogero Sperandeo
- * 
- * by jim_bug:)
- * */
+
 int main(){
     pid_t pid;
     while (1){
+        int stateBackground = 0;
         char path[MAX_LENGTH_PATH];
         getcwd(path, sizeof(path));
         printf("jsh:\"%s\">", path);
@@ -31,35 +35,49 @@ int main(){
             token = strtok(NULL, " \n");
             length ++;
         }
-        argv[length] = NULL;
-	if(strcmp(argv[0], "exit") == 0){ 	// caso di uscita
-		exit(0);
-	}
-        pid = fork();   // creo un nuovo processo
-        if(pid == 0){   // effettuo la system call dentro il processo figlio.
-            if(strcmp(argv[0], "cd") == 0){    // se voglio cambiare directory
-                if(strcmp(argv[1], "~") == 0) {     // carettere speciale che cambia la posizione in /home/user
-                    char *userName = getlogin();        // ottengo il nome dell'utente corrente
-                    char userPath[MAX_LENGTH_USER_PATH] = "/home/";
-                    strcat(userPath, userName);     // concateno le due stringhe in modo tale da avere un path valido
-                    strcpy(argv[1], userPath);     // assegno la path alla nuova directory
-//                    chdir(argv[1]);        // cambio posizione nel file system
-                }
-                if(chdir(argv[1]) != 0){    // cambio posizione nel file system e verifico se ha generato qualche errore.
-                    perror("ERROR");
-                }
+
+        if(strcmp(argv[length-1], "&") == 0){
+            stateBackground = 1;
+            argv[length-1] = NULL;
+        }
+        else{
+            argv[length] = NULL;
+        }
+
+
+        if(strcmp(argv[0], "exit") == 0){ 	// caso di uscita
+            printf("SONO QUI\n");
+            break;
+        }
+
+        // gestisco il cambio di directory nel processo padre in quanto farlo nel processo figlio non rifletterà nel processo padre.
+        if(strcmp(argv[0], "cd") == 0){
+            if(strcmp(argv[1], "+") == 0) {             // carettere speciale che cambia la posizione in /home/user
+                char *userName = getlogin();            // ottengo il nome dell'utente corrente
+                char userPath[MAX_LENGTH_USER_PATH] = "/home/";
+                strcat(userPath, userName);             // concateno le due stringhe in modo tale da avere un path valido
+                strcpy(argv[1], userPath);              // assegno la path alla nuova directory
             }
-            else{
-//		printf("%s", argv[0]);
-                execvp(argv[0], argv);
+            if(chdir(argv[1]) != 0){    // cambio posizione nel file system e verifico se ha generato qualche errore.
+                perror("ERROR");
+            }
+            continue;
+        }
+
+        pid = fork();
+        if(pid == 0){
+            if(execvp(argv[0], argv) == -1){
+                perror("COMMAND NOT FOUND!");
+                exit(-1);
             }
         }
         else {
-            wait(NULL);     // aspetto che il processo figlio termini
-
-
+            if(!stateBackground){
+                wait(NULL);         // il processo padre attenderà il processo figlio che termini.
+            }
         }
     }
 
     return 0;
 }
+// :)
