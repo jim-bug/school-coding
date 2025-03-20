938bd75b605df46119ecdc8aa2d836062aff9fe1
jim_bug <ignazioandsperandeo@gmail.com>
2025-03-20 01:24:02 +0100

    .

---
+++ Php/CreazioneDB_PHP/index.php
@@ -0,0 +1,12 @@
+/*
+Autore: Ignazio Leonardo Calogero Sperandeo
+Data: 20/03/2025
+Consegna: Sfruttando PHP con i PDO create un database, create 3 tabelle all'interno del DB stesso e gestite le eccezioni. 
+Provate a vedere autonomamente come effettuare delle interrogazioni SQL tramite prepared statements e i bindParam ed eseguitele nel codice PHP.
+
+by jim_bug // :)
+*/ 
+<?php
+
+
+?>
