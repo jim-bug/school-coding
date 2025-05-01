c27b507b55bfb7ada8d6767abf36d20a25a37ca5
jim_bug <ignazioandsperandeo@gmail.com>
2025-05-01 12:46:18 +0200

    .

--- Php/webapp.php
+++ Php/webapp.php
@@ -2,7 +2,7 @@
 
 $hostname = 'localhost';
 $username = 'root';
-$password = 'rotoremindme';
+$password = 'bazinga ops :)';
 $database = 'GestioneRisorseSperandeo';
 
 $mysql = new mysqli($hostname, $username, $password, $database);
